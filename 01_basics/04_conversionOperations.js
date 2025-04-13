8acf1d5530608972983c503c28f4cca221b5f532
Abhishek-S-13 <abhisheks1302@gmail.com>
2025-04-13 14:27:59 +0000

    conversion Operations

--- 01_basics/04_conversionOperations.js
+++ 01_basics/04_conversionOperations.js
@@ -38,3 +38,42 @@ console.log(typeof stringNumber); // string
     33
     string
 */
+
+/* ********************** Operations ********************** */
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+/* -3 */
+
+/* Example 2 */
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2**2); // 4
+console.log(4/2); // 2
+console.log(2%3); // 2
+
+/* Example 3 */
+
+let str1 = "Hi"
+let str2 = "Abhi"
+let str3 = str1 + str2 
+console.log(str3); // Hi Abhi
+
+/* Example 4 */ // 
+
+console.log( "1" + 2 );         // 12
+console.log( 1 + "2" );         // 12
+console.log( "1" + 2 + 2 );     // 122
+console.log( 1 + 2 + "2" );     // 32 - https://tc39.es/ecma262/#sec-toprimitive - check this 
+
+console.log(true);  // true
+console.log(+true); // 1
+console.log(+"");   // 0
+
+/* Study link 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
+
+
