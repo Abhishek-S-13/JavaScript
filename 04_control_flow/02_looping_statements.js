03717985639d43ca77a9f7281a4f8ab06bdb1eb4
Abhishek-S-13 <abhisheks1302@gmail.com>
2025-04-18 06:42:47 +0000

    looping_statements

---
+++ 04_control_flow/02_looping_statements.js
@@ -0,0 +1,45 @@
+/* 2. Looping Statements (Iteration) */
+
+/* ************************************** for loop: ************************************** */
+for (let i = 0; i < 5; i++) {
+    console.log("For Loop "+i);
+}
+/* OP
+  For Loop 0
+  For Loop 1
+  For Loop 2
+  For Loop 3
+*/
+
+/* ************************************** while loop ************************************** */
+let j = 0;
+while (j < 5) {
+  console.log("While " +j);
+  j++;
+}
+/* OP:
+While 0
+While 1
+While 2
+While 3
+While 4
+*/
+
+/* ************************************** do...while loop: ************************************** */
+let k = 0;
+do {
+  console.log("Do While "+k);
+  k++;
+} while (k < 5);
+
+/* OP
+Do While 0
+Do While 1
+Do While 2
+Do While 3
+Do While 4
+*/
+
+
+/* ************************************************************************************** */
+/* Source from videos */
