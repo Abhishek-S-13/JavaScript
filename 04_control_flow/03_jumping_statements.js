953f5b70be6dcfa26d7ea32df62def84d5011931
Abhishek-S-13 <abhisheks1302@gmail.com>
2025-04-18 12:33:21 +0000

    jumping_statements

--- 04_control_flow/03_jumping_statements.js
+++ 04_control_flow/03_jumping_statements.js
@@ -4,14 +4,14 @@
 
 for (let i = 0; i < 10; i++) {
   if (i === 5) break;
-  console.log("For Loop Break"+i);
+  console.log("For Loop Break "+i);
 }
 /* OP
-For Loop Break0
-For Loop Break1
-For Loop Break2
-For Loop Break3
-For Loop Break4
+For Loop Break 0
+For Loop Break 1
+For Loop Break 2
+For Loop Break 3
+For Loop Break 4
 */
 /* ************************************** continue: ************************************** */
 
@@ -33,3 +33,44 @@ function sum(a, b) {
 }
 console.log(sum(2,4));
 // 6 
+
+/* ***************************** Source from videos Break and Continue ************************ */
+/* BREAK Example */
+for(let i = 1; i <= 6 ; i++){
+  if(i==3){
+    console.log(`Detect 3`);
+    break;
+  }
+  console.log(`Value of i is ${i}`);
+}
+/* OP: for adding if condition
+Value of i is 1
+Value of i is 2
+Detect 3
+*/
+
+/* OP for without if condition 
+Value of i is 1
+Value of i is 2
+Value of i is 3
+Value of i is 4
+Value of i is 5
+Value of i is 6
+*/
+
+/* CONTINUE Example */
+for(let i = 1; i <= 6 ; i++){
+  if(i==3){
+    console.log(`Detect 3`);
+    continue;
+  }
+  console.log(`Value of i is ${i}`);
+}
+/*
+  Value of i is 1
+  Value of i is 2
+  Detect 3
+  Value of i is 4
+  Value of i is 5
+  Value of i is 6
+*/
